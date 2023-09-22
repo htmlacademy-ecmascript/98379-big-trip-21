c6683aab74071d6b18787c8c508898bf36837b94
unknown <robi90@mail.ru>
2023-09-22 19:52:33 +0300

    Горящий поезд 3. закончил отрисовку моков в точках маршрута

--- src/view/header-time-filters-view.js
+++ src/view/header-time-filters-view.js
@@ -3,7 +3,7 @@ import AbstractView from '../framework/view/abstract-view.js';
 function createFilterTemplate() {
   return `<form class="trip-filters" action="#" method="get">
         <div class="trip-filters__filter">
-          <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything">
+          <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
           <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
         </div>
         <div class="trip-filters__filter">
@@ -17,7 +17,7 @@ function createFilterTemplate() {
         </div>
 
         <div class="trip-filters__filter">
-          <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past" checked>
+          <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
           <label class="trip-filters__filter-label" for="filter-past">Past</label>
         </div>
 
